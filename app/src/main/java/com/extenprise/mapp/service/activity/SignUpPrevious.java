4ca31e49e6d97a6e15c5ed9a8689102259a4228a
sanjana <kale_sanjana@extenprise.com>
2015-08-26 16:26:00 +0530

    Design New Layout

--- app/src/main/java/com/extenprise/mapp/service/activity/SignUpPrevious.java
+++ app/src/main/java/com/extenprise/mapp/service/activity/SignUpPrevious.java
@@ -2,6 +2,7 @@ package com.extenprise.mapp.service.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -33,7 +34,9 @@ public class SignUpPrevious extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sign_up_previous, menu);
+        //getMenuInflater().inflate(R.menu.menu_sign_up_previous, menu);
+        //return true;
+        getMenuInflater().inflate(R.menu.menu_search_doctor, menu);
         return true;
     }
 
@@ -42,13 +45,26 @@ public class SignUpPrevious extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /*int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.action_search:
+                Intent intent = new Intent(this, SearchDoctorActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_sign_in:
+                return true;
+        }
         return super.onOptionsItemSelected(item);
+       // return super.onOptionsItemSelected(item);
     }
 }
