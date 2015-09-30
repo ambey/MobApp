6e4dc0b8402234fc7ab9d9c5eff7f89cec67e7d6
avinash <jain_avinash@extenprise.com>
2015-09-30 14:49:22 +0530

    Serv Prov View Profile.

--- app/src/main/java/com/extenprise/mapp/service/activity/ServProvViewWorkDetails.java
+++ app/src/main/java/com/extenprise/mapp/service/activity/ServProvViewWorkDetails.java
@@ -1,11 +1,29 @@
 package com.extenprise.mapp.service.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
+import com.extenprise.mapp.LoginHolder;
 import com.extenprise.mapp.R;
+import com.extenprise.mapp.db.MappContract;
+import com.extenprise.mapp.service.data.ServProvHasService;
+import com.extenprise.mapp.service.data.Service;
+import com.extenprise.mapp.service.data.ServicePoint;
+import com.extenprise.mapp.service.data.ServiceProvider;
+import com.extenprise.mapp.util.SearchServProv;
+import com.extenprise.mapp.util.UIUtility;
+
+import java.util.Calendar;
 
 
 public class ServProvViewWorkDetails extends Activity {
@@ -14,6 +32,106 @@ public class ServProvViewWorkDetails extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servprov_workdetail);
+
+        final Cursor cursor = SearchServProv.getCursor();
+
+        /* SimpleCursorAdapter(Context context,
+                    int layout,
+                    Cursor c,
+                    String[] from,
+                    int[] to,
+                    int flags);*/
+
+
+        //ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.activity_search_result);
+
+
+        String[] values = new String[] {
+                MappContract.ServiceProvider.COLUMN_NAME_FNAME,
+                MappContract.ServiceProvider.COLUMN_NAME_LNAME,
+                MappContract.ServProvHasServ.COLUMN_NAME_SPECIALITY,
+                MappContract.ServProvHasServ.COLUMN_NAME_EXPERIENCE,
+                MappContract.ServicePoint.COLUMN_NAME_NAME,
+                MappContract.ServicePoint.COLUMN_NAME_LOCATION
+        };
+
+        int[] viewIds = new int[] {
+                R.id.viewFirstName,
+                R.id.viewLastName,
+                R.id.viewDocSpeciality,
+                R.id.viewExpValue,
+                R.id.viewClinicName,
+                R.id.viewLocation
+        };
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                R.layout.activity_search_result,
+                cursor,
+                values,
+                viewIds, 0){
+            @Override
+            public View getView(int position, View convertView,
+                                ViewGroup parent) {
+
+                View view =super.getView(position, convertView, parent);
+                cursor.moveToPosition(position);
+                String docAvailDays = cursor.getString(cursor.getColumnIndex(MappContract.ServProvHasServHasServPt.COLUMN_NAME_WEEKLY_OFF));
+
+                ImageView mImageViewAvailable = (ImageView) view.findViewById(R.id.imageViewAvailability);
+
+                if(UIUtility.findDocAvailability(docAvailDays, Calendar.getInstance())) {
+                    mImageViewAvailable.setImageResource(R.drawable.gcircle);
+                } else {
+                    mImageViewAvailable.setImageResource(R.drawable.rcircle);
+                }
+
+            /*YOUR CHOICE OF COLOR*/
+                //textView.setTextColor(Color.BLUE);
+
+                return view;
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.docListView);
+        listView.setDescendantFocusability(ListView.FOCUS_BLOCK_DESCENDANTS);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                cursor.moveToPosition(position);
+
+                ServiceProvider sp = new ServiceProvider();
+                sp.setIdServiceProvider(cursor.getInt(cursor.getColumnIndex(MappContract.ServiceProvider._ID)));
+                sp.setfName(cursor.getString(cursor.getColumnIndex(MappContract.ServiceProvider.COLUMN_NAME_FNAME)));
+                sp.setlName(cursor.getString(cursor.getColumnIndex(MappContract.ServiceProvider.COLUMN_NAME_LNAME)));
+                sp.setQualification(cursor.getString(cursor.getColumnIndex(MappContract.ServiceProvider.COLUMN_NAME_QUALIFICATION)));
+
+                Service s = new Service();
+                s.setSpeciality(cursor.getString(cursor.getColumnIndex(MappContract.ServProvHasServ.COLUMN_NAME_SPECIALITY)));
+
+                ServProvHasService sps = new ServProvHasService();
+                sps.setServProv(sp);
+                sps.setExperience(Float.parseFloat(cursor.getString(cursor.getColumnIndex(MappContract.ServProvHasServ.COLUMN_NAME_EXPERIENCE))));
+                sps.setService(s);
+
+                ServicePoint spt = new ServicePoint();
+                spt.setName(cursor.getString(cursor.getColumnIndex(MappContract.ServicePoint.COLUMN_NAME_NAME)));
+                spt.setLocation(cursor.getString(cursor.getColumnIndex(MappContract.ServicePoint.COLUMN_NAME_LOCATION)));
+
+                LoginHolder.spsspt.setConsultFee(Float.parseFloat(cursor.getString(cursor.getColumnIndex(MappContract.ServProvHasServHasServPt.COLUMN_NAME_CONSULTATION_FEE))));
+                LoginHolder.spsspt.setStartTime(cursor.getInt(cursor.getColumnIndex(MappContract.ServProvHasServHasServPt.COLUMN_NAME_START_TIME)));
+                LoginHolder.spsspt.setEndTime(cursor.getInt(cursor.getColumnIndex(MappContract.ServProvHasServHasServPt.COLUMN_NAME_END_TIME)));
+                LoginHolder.spsspt.setWeeklyOff(cursor.getString(cursor.getColumnIndex(MappContract.ServProvHasServHasServPt.COLUMN_NAME_WEEKLY_OFF)));
+                LoginHolder.spsspt.setServPointType(cursor.getString(cursor.getColumnIndex(MappContract.ServProvHasServHasServPt.COLUMN_NAME_SERVICE_POINT_TYPE)));
+                LoginHolder.spsspt.setServProvHasService(sps);
+                LoginHolder.spsspt.setServicePoint(spt);
+
+                Intent i = new Intent(getApplicationContext(), ServProvDetailsActivity.class);
+                startActivity(i);
+            }
+        });
+        listView.setAdapter(adapter);
     }
 
     @Override
